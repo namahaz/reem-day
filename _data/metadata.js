4b8356d01f8bedb2b291e85d3c6b4e470dcc6edb
Vikram Rojo <149986153+avt1ak@users.noreply.github.com>
2023-12-01 10:17:04 -0700

    og test

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Dec 23rd, A Day For Reem",
 	url: "https://reem.day/",
 	language: "en",
-	description: "Reem's Day of Rememberance for Children of Wars, Reem and Tarek Ali Badwan Grandchildren of Khaled Nabhan",
+	description: "December 23rd as Reem's Day of Rememberance for Children of Wars, Reem and Tarek Ali Badwan, Children of Maysa and Grandchildren of Khaled Nabhan",
 	toll: 8241,
 	author: {
 		name: "Reem.Day",
